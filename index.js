d3d35bc8a5325164e7003201d54e0a975925777b
adar7295 <ssrivastav7295@gmail.com>
2024-04-17 15:29:39 +0530

    api

--- index.js
+++ index.js
@@ -5,5 +5,22 @@ const express = require('express')
 const app = express();
 const port = 5000;
 
+//end point
+app.get('/', (req, res) => {
+    res.send('response from expess');
+});
+
+app.get('/add', (req, res)=> {
+    res.send('Response from add');
+});
+
+//delete
+app.get('/delete', (req, res) => {
+    res.send('response delete'); 
+});
+
+app.get('/update', (req, res) => {
+    res.send('response update'); 
+});
 // start server
 app.listen(port, () => { console.log('server started'); });
